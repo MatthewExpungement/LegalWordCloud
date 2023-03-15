ed69598dceec2df2cc4093fcf89c60917e97efb0
Matthew Stubenberg <matthewstubenberg@gmail.com>
2023-03-15 13:08:29 -1000

    Added Gavel image

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # LegalWordCloud
  Generates Word Clouds based on legal opinions
-#wordcloud @ file:///wordcloud-1.8.1-cp311-cp311-win_amd64.whl
--- streamlit_app.py
+++ streamlit_app.py
@@ -5,49 +5,15 @@ import matplotlib.pyplot as plt
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
-
-# if not os.path.isdir("/tmp/wc-lib"):
-
-#     # Download wordcloud to disk
-#     with open("/tmp/wordcloud-1.8.1-cp39-cp39-win_amd64.whl", "wb") as file:
-#         response = requests.get(
-#             "https://download.lfd.uci.edu/pythonlibs/archived/wordcloud-1.8.1-cp39-cp39-win_amd64.whl"
-#         )
-#         file.write(response.content)
-#     # get our current dir, to configure it back again. Just house keeping
-#     default_cwd = os.getcwd()
-#     os.chdir("/tmp")
-#     # untar
-#     # build
-#     os.system("./configure --prefix=/home/appuser")
-#     os.system("make")
-#     # install
-#     os.system("make install")
-#     # back to the cwd
-#     os.chdir(default_cwd)
-#     sys.stdout.flush()
-
-# # add the library to our current environment
-# from ctypes import *
-
-# lib = CDLL("/home/appuser/lib/libta_lib.so.0.0.0")
-# # import library
-# try:
-#     from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-# except ImportError:
-#     subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "tmp"])
-# finally:
-#     from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-
-
-
-
+import os
+import numpy as np
 
 def reset_justia_url():
     st.session_state['url_search'] = ""
 def reset_select_box_option():
     st.session_state.dropdown_search = "Select"
 
+# Generates a sidebar
 with st.sidebar:
     st.title("Welcome to the Legal Opinion Word Cloud Generator")
     st.write("Enter the url from Justia of the opinion you want to word cloud .i.e. https://supreme.justia.com/cases/federal/us/388/1/")
@@ -55,46 +21,44 @@ with st.sidebar:
     st.write("Or select from famous cases")
     famous_cases = ['Select','Loving v. Virginia, 388 U.S. 1 (1967)','Roe v. Wade, 410 U.S. 113 (1973)','Brown v. Board of Education of Topeka, 347 U.S. 483 (1954)']
     famous_case = st.selectbox("Select Famous Case",famous_cases,key='dropdown_search',on_change=reset_justia_url)
-    exclude_words_basics = ["state",'footnote','page','statute','court','code ann','case','statutes','repl vol','act','v','Â','ann']
+    exclude_words_basics = ["state",'footnote','page','statute','court','code ann','case','statutes','repl vol','act','v','Â','ann','F Supp']
     exclude_words_raw = st.text_area("Words to exclude",value=",".join(exclude_words_basics))
     exclude_words = exclude_words_raw.split(",")
     number_of_words = st.slider("How many words should the word cloud be capped at?",0,1000,50)
+    images = ['Rectangle','Heart','Gavel']
+    selected_image = st.selectbox("Select Background Image",images)
     st.text("The word cloud will generate on change of any attribute above.")
 if(url != "" or famous_case != 'Select'):
     if(famous_case != 'Select'):
         #Means we have a pre-selected case.
-        
-
         my_file = open("Cases//" + famous_case + ".txt")
         text = my_file.read()
         my_file.close()
         case_title = famous_case
     else:
         #Means it's a url search request
-        
         response = requests.get(url)
         #print(response.text)
         soup = BeautifulSoup(response.text)
         case_title = soup.find(id = 'text-a').get_text()
         text = soup.find(id='tab-opinion').get_text()
-    st.header(case_title)
-
-    #Create and generate a word cloud image:
-    # d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-    # alice_coloring = np.array(Image.open(path.join(d, "heart2.jpg")))
-    # wordcloud = WordCloud(background_color="white", max_words=500, mask=alice_coloring,stopwords=stopwords, max_font_size=40, random_state=42).generate(text)
-    # image_colors = ImageColorGenerator(alice_coloring)
-    # plt.imshow(wordcloud.recolor(color_func=image_colors), interpolation="bilinear")
+    st.header(case_title) #Print the title
 
     stopwords = set(STOPWORDS)
     for sword in exclude_words:
         stopwords.add(sword)
 
-    wordcloud = WordCloud(max_words=number_of_words,stopwords=stopwords, max_font_size=40).generate(text)
+    if(selected_image == 'Rectangle'):
+        #Regular word cloud
+        wordcloud = WordCloud(background_color="white",max_words=number_of_words,stopwords=stopwords, max_font_size=40).generate(text)
+        plt.imshow(wordcloud, interpolation='bilinear')
+    else:
+        #Create and generate a word cloud image:
+        d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+        image_coloring = np.array(Image.open(path.join(d, "Images/" + selected_image + ".jpg")))
+        wordcloud = WordCloud(background_color="white", max_words=500, mask=image_coloring,stopwords=stopwords, max_font_size=40, random_state=42).generate(text)
+        image_colors = ImageColorGenerator(image_coloring)
+        plt.imshow(wordcloud.recolor(color_func=image_colors), interpolation="bilinear")
 
-    # Display the generated image:
-    plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
-
     st.pyplot(plt)
-    #plt.show() #If not running on streamlit
